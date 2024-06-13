b9e354b18ada096053dcbbd15a61edc4319843ec
nathansebby <sebbynathan7@gmail.com>
2024-06-13 15:25:49 +0300

    20

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from lib.cli import main_menu
 from lib.models import Team, Jersey
 
 if __name__ == "__main__":
-    # Create tables if they don't exist
+   
     Team.create_table()
     Jersey.create_table()
     
-    # Start the CLI
+    print("\nWelcome to the Team!")
     main_menu()
